7c34d9108a6e18f2edaad8430eab483b55347e8f
VitorCioletti <ciolettiv@gmail.com>
2021-04-18 00:19:21 -0300

    Adicionada regra da jogada ComprarCarta.

--- Entidades/Jogadas/Tipos/ComprarCarta.cs
+++ Entidades/Jogadas/Tipos/ComprarCarta.cs
@@ -1,12 +1,20 @@
 namespace ServidorPiratas.Entidades.Jogadas.Tipos
 {
+    using System;
+
     public class ComprarCarta : Jogada
     {
         public ComprarCarta(Jogador jogador) : base(jogador) { }
 
         public override void AplicaRegra(Mesa mesa)
         {
+            var quantidadeMaximaCartas = 10;
+            var cartasNaMao = Realizador.CartasNaMao;
 
+            if (cartasNaMao.Count < quantidadeMaximaCartas)
+                cartasNaMao.Add(mesa.BaralhoCentral.ObtemTopo());
+            else
+                throw new Exception("Limite de cartas na mão atingido.");
         }
     }
 }

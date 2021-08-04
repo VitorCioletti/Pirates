f8a0f9c7ce596dc4b163c8ee3a80b8231703c393
VitorCioletti <ciolettiv@gmail.com>
2021-08-04 19:54:13 -0300

    Corrigida regra de cartas resposta ao responder duelo.

--- Regras/Acoes/Resultantes/ResponderDuelo.cs
+++ Regras/Acoes/Resultantes/ResponderDuelo.cs
@@ -2,21 +2,28 @@ namespace ServidorPiratas.Regras.Acoes.Resultantes
 {
     using Cartas.Tipos;
     using Regras;
+    using System;
+    using System.Collections.Generic;
     using Tipos;
 
     public class ResponderDuelo : Resultante
     {
+        private int _limiteCartasResposta = 2;
+
         public Jogador Vitorioso { get; private set; }
 
         public Jogador Perdedor { get; private set; }
 
-        public Duelo CartaResposta { get; private set; }
+        public List<Duelo> CartasResposta { get; private set; }
 
         public ResponderDuelo(Jogador realizador, Jogador alvo) : base(realizador, alvo) {}
 
         public override Resultante AplicarRegra(Mesa mesa)
         {
-            CartaResposta?.AplicarEfeito(this, mesa);
+            if (CartasResposta.Count > _limiteCartasResposta)
+                throw new Exception("Limite de cartas resposta atigindo.");
+
+            CartasResposta.ForEach(c => c.AplicarEfeito(this, mesa));
 
             Vitorioso = Realizador.Campo.CalcularPontosDuelo() > Alvo.Campo.CalcularPontosDuelo() ? Realizador : Alvo;
             Perdedor = Vitorioso == Realizador ? Realizador : Alvo;

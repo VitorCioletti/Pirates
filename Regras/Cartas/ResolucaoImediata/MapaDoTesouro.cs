ee3daa2705176f1e088c9886d3ff86cd4cff4673
VitorCioletti <ciolettiv@gmail.com>
2021-08-04 20:16:26 -0300

    Adicionado campo em mapa do tesouro para configurar cartas obtidas.

--- Regras/Cartas/ResolucaoImediata/MapaDoTesouro.cs
+++ Regras/Cartas/ResolucaoImediata/MapaDoTesouro.cs
@@ -8,13 +8,15 @@ namespace ServidorPiratas.Regras.Cartas.ResolucaoImediata
 
     public class MapaDoTesouro : ResolucaoImediata
     {
+        private int _cartasObtidas = 4;
+
         public MapaDoTesouro(string nome) : base(nome) {}
 
         public override Resultante AplicarEfeito(Acao acao, Mesa mesa) => _aplicarEfeito(acao, mesa.BaralhoCentral);
 
         internal Resultante _aplicarEfeito(Acao acao, BaralhoCentral baralhoCentral)
         {
-            var cartasOpcoes = baralhoCentral.ObterTopo(4);
+            var cartasOpcoes = baralhoCentral.ObterTopo(_cartasObtidas);
 
             return new EscolherCartaBaralho(acao.Realizador, baralhoCentral, cartasOpcoes);
         }

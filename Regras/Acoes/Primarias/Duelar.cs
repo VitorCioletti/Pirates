87d1733bbcb6269b7c50078df0b7eeb717e1c66b
VitorCioletti <ciolettiv@gmail.com>
2021-08-04 19:58:09 -0300

    Adicionada restrição de timoneiro ao declarar um duelo.

--- Regras/Acoes/Primarias/Duelar.cs
+++ Regras/Acoes/Primarias/Duelar.cs
@@ -1,15 +1,23 @@
 namespace ServidorPiratas.Regras.Acoes.Primarias
 {
+    using System;
     using Acoes.Resultantes;
+    using Cartas.Duelo;
+    using Cartas.Tipos;
     using Regras;
     using Tipos;
 
     public class Duelar: Primaria
     {
+        public Duelo CartaIniciadora { get; private set; }
+
         public Duelar(Jogador realizador, Jogador alvo) : base(realizador, alvo) {}
 
         public override Resultante AplicarRegra(Mesa mesa)
         {
+            if (CartaIniciadora is Timoneiro)
+                throw new Exception($"\"{nameof(Timoneiro)}\" não pode iniciar um Duelo.");
+
             mesa.EntrarModoDuelo(Realizador, Alvo);
 
             return new ResponderDuelo(Alvo, Realizador);

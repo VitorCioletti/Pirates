f606a5c90b619aebaa730638a693a8e2f82758e9
VitorCioletti <ciolettiv@gmail.com>
2022-03-06 21:27:28 -0300

    orrigido editor config para underline em campos.

--- Piratas.Servidor/Piratas.Servidor.Dominio/Cartas/Tesouro/MeioAmuleto.cs
+++ Piratas.Servidor/Piratas.Servidor.Dominio/Cartas/Tesouro/MeioAmuleto.cs
@@ -5,17 +5,17 @@ namespace Piratas.Servidor.Dominio.Cartas.Tesouro
 
     public class MeioAmuleto : Tesouro
     {
-        private static readonly int QuantidadeParaCompletar = 2;
+        private static readonly int _quantidadeParaCompletar = 2;
 
-        private static readonly int ValorAmuletoCompleto = 2;
+        private static readonly int _valorAmuletoCompleto = 2;
 
         public MeioAmuleto(string nome) : base(nome, 0) { }
 
         public static int CalcularPontosTesouro(List<MeioAmuleto> amuletos)
         {
-            var amuletosCompletos = amuletos.Count / QuantidadeParaCompletar;
+            var amuletosCompletos = amuletos.Count / _quantidadeParaCompletar;
 
-            return amuletosCompletos * ValorAmuletoCompleto;
+            return amuletosCompletos * _valorAmuletoCompleto;
         }
     }
 }

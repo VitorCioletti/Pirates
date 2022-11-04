a0d12c6f343dddfacf415677323080789a2174a6
VitorCioletti <ciolettiv@gmail.com>
2022-11-04 11:45:47 -0300

    Formatado id da carta para kebab-case.

--- Servidor/Piratas.Servidor.Dominio/Cartas/Carta.cs
+++ Servidor/Piratas.Servidor.Dominio/Cartas/Carta.cs
@@ -1,13 +1,22 @@
 namespace Piratas.Servidor.Dominio.Cartas
 {
     using System.Collections.Generic;
+    using System.Globalization;
+    using System.Text.RegularExpressions;
     using Acoes;
 
     public abstract class Carta
     {
         public string Id { get; protected set; }
 
-        protected Carta() => Id = GetType().ToString();
+        protected Carta()
+        {
+            string nomeTipo = GetType().ToString();
+
+            string id = Regex.Replace(nomeTipo, "(?<!^)([A-Z][a-z]|(?<=[a-z])[A-Z0-9])", "-$1", RegexOptions.Compiled);
+
+            Id = id.ToLower(CultureInfo.InvariantCulture);
+        }
 
         public abstract List<BaseAcao> AplicarEfeito(BaseAcao baseAcao, Mesa mesa);
     }

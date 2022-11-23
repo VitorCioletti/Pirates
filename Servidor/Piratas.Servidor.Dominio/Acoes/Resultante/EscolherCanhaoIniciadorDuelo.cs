1764982c8063625f0859ddfd5bbd0b190583ddf2
VitorCioletti <ciolettiv@gmail.com>
2022-11-22 22:52:55 -0300

    Corrigidos erros de compilação.

--- Servidor/Piratas.Servidor.Dominio/Acoes/Resultante/EscolherCanhaoIniciadorDuelo.cs
+++ Servidor/Piratas.Servidor.Dominio/Acoes/Resultante/EscolherCanhaoIniciadorDuelo.cs
@@ -1,41 +1,42 @@
-namespace Piratas.Servidor.Dominio.Acoes.Resultante;
-
-using System.Collections.Generic;
-using Base;
-using Cartas.Duelo;
-using Cartas.Tipos;
-using Enums;
-using Imediata;
-
-public class EscolherCanhaoIniciadorDuelo : BaseResultanteComListaEscolhas
+namespace Piratas.Servidor.Dominio.Acoes.Resultante
 {
-    public EscolherCanhaoIniciadorDuelo(
-        BaseAcao origem,
-        Jogador realizador,
-        TipoEscolha tipoEscolha,
-        List<string> opcoes)
-        : base(
-            origem,
-            realizador,
-            tipoEscolha,
-            opcoes)
+    using System.Collections.Generic;
+    using Base;
+    using Cartas.Duelo;
+    using Cartas.Tipos;
+    using Enums;
+    using Imediata;
+
+    public class EscolherCanhaoIniciadorDuelo : BaseResultanteComListaEscolhas
     {
-    }
+        public EscolherCanhaoIniciadorDuelo(
+            BaseAcao origem,
+            Jogador realizador,
+            TipoEscolha tipoEscolha,
+            List<string> opcoes)
+            : base(
+                origem,
+                realizador,
+                tipoEscolha,
+                opcoes)
+        {
+        }
 
-    public override List<BaseAcao> AplicarRegra(Mesa mesa)
-    {
-        var canhaoIniciador = (Canhao)Realizador.Mao.ObterPorId(Opcoes[0]);
+        public override List<BaseAcao> AplicarRegra(Mesa mesa)
+        {
+            var canhaoIniciador = (Canhao)Realizador.Mao.ObterPorId(Opcoes[0]);
 
-        canhaoIniciador.AplicarEfeito(this, mesa);
+            canhaoIniciador.AplicarEfeito(this, mesa);
 
-        mesa.EntrarModoDuelo(Realizador, Alvo);
+            mesa.EntrarModoDuelo(Realizador, Alvo);
 
-        BaseAcao proximaAcao = !Alvo.Mao.Possui<Duelo>()
-            ? new CalcularResultadoDuelo(Realizador, Alvo)
-            : new DescerCartaRespostaDuelo(this, Alvo, Realizador);
+            BaseAcao proximaAcao = !Alvo.Mao.Possui<Duelo>()
+                ? new CalcularResultadoDuelo(Realizador, Alvo)
+                : new DescerCartaRespostaDuelo(this, Alvo, Realizador);
 
-        var acoesResultantes = new List<BaseAcao> { proximaAcao };
+            var acoesResultantes = new List<BaseAcao> {proximaAcao};
 
-        return acoesResultantes;
+            return acoesResultantes;
+        }
     }
 }
--- Servidor/Piratas.Servidor.Dominio/Excecoes/Acoes/NaoPossuiCartaDueloExcecao.cs
+++ Servidor/Piratas.Servidor.Dominio/Excecoes/Acoes/NaoPossuiCartaDueloExcecao.cs
@@ -1,14 +1,15 @@
-namespace Piratas.Servidor.Dominio.Excecoes.Acoes;
-
-using Dominio.Acoes;
-
-public class NaoPossuiCartaDueloExcecao : BaseAcoesExcecao
+namespace Piratas.Servidor.Dominio.Excecoes.Acoes
 {
-    public NaoPossuiCartaDueloExcecao(BaseAcao acao)
-        : base(
-            acao,
-            "nao-possui-carta-duelo",
-            $"Jogador \"{acao.Realizador.Id}\" não possui carta de duelo.")
+    using Dominio.Acoes;
+
+    public class NaoPossuiCartaDueloExcecao : BaseAcoesExcecao
     {
+        public NaoPossuiCartaDueloExcecao(BaseAcao acao)
+            : base(
+                acao,
+                "nao-possui-carta-duelo",
+                $"Jogador \"{acao.Realizador.Id}\" não possui carta de duelo.")
+        {
+        }
     }
 }

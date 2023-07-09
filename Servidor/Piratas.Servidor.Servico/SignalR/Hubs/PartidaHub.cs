62ae8e0f67729f73a8b9243a8e7f67124cc76c21
VitorCioletti <ciolettiv@gmail.com>
2023-07-09 19:36:12 -0300

    Removida espera de tasks de envio no PartidsHub.

--- Servidor/Piratas.Servidor.Servico/SignalR/Hubs/PartidaHub.cs
+++ Servidor/Piratas.Servidor.Servico/SignalR/Hubs/PartidaHub.cs
@@ -1,7 +1,6 @@
 namespace Piratas.Servidor.Servico.SignalR.Hubs;
 
 using System.Collections.Generic;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
 using Partida;
 using Protocolo.Partida.Cliente;
@@ -14,17 +13,11 @@ public class PartidaHub : Hub
         List<MensagemPartidaServidor> mensagensServidor =
             GerenciadorPartidaServico.ProcessarMensagemCliente(mensagemPartidaCliente);
 
-        List<Task> allSendAsyncTasks = new();
-
         foreach (MensagemPartidaServidor mensagemPartidaServidor in mensagensServidor)
         {
             string idJogadorRealizador = mensagemPartidaServidor.IdJogadorRealizador;
 
-            Task sendAsync = Clients.Client(idJogadorRealizador).SendAsync("AoProcessarMensagem", mensagensServidor);
-
-            allSendAsyncTasks.Add(sendAsync);
+            Clients.Client(idJogadorRealizador).SendAsync("AoProcessarMensagem", mensagensServidor);
         }
-
-        Task.WhenAll(allSendAsyncTasks);
     }
 }
